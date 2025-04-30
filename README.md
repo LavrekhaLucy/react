337b01e8e803f48e7101d691d84e6445745577fd
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-04-30 13:23:12 +0300

    hw3/qL0b5uh

--- README.md
+++ README.md
@@ -52,3 +52,16 @@ export default tseslint.config({
   },
 })
 ```
+Побудувати меню, з можливістю переходу на наступні маршрути:
+
+/users/jsonplaceholder
+
+/users/dummyjson
+
+/posts/jsonplaceholder
+
+/posts/dummyjson
+
+/comments/jsonplaceholder
+
+Контент на сторінках відсутній. Лише грамотна обробка роутів
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import './App.css'
-import {products} from "./data/productsList.ts";
-import MyProduct from "./MyComponents/my-product/MyProduct.tsx";
-
 
 
 function App() {
 
 
   return (
-    <div className={'foo'}>
-        {
-          products.map( (product, index ) => <MyProduct key={index} product = {product}/>
-         )
-        }
-    </div>
+    <>
+
+    </>
   )
 }
 
