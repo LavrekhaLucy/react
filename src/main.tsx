4e597e56b8523f6c3ef1a8789e013900514957ff
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-05-19 19:19:33 +0300

    lesson 8

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,35 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider} from "react-router-dom";
 import {routes} from "./route/routes.tsx";
+import {Provider} from "react-redux";
+import {configureStore, createSlice} from "@reduxjs/toolkit";
+import {IUser} from "./models/IUser.ts";
 
+type userSliceType = {
+    users:IUser[];
+}
+
+
+const initialState:userSliceType = {users:[]};
+
+const userSlice = createSlice({
+    name: "userSlice",
+    initialState: initialState,
+    reducers: {}
+})
+
+const store = configureStore({
+    reducer: {
+        userSlice:userSlice.reducer,
+        // postSlice:null
+    }
+});
 
     createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={routes} />)
+    .render(
+        <Provider store={store}>
+            <RouterProvider router={routes} />
+        </Provider>
+
+    )
 
